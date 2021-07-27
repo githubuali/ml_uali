ea0f47d4cdaa11760542318a3539ca54ba94f9b9
mantiagosartinez <samartin1995@gmail.com>
2021-07-27 15:08:00 -0300

    Otra prueba de pusheo

---
+++ script/auxiliar.py
@@ -0,0 +1,28 @@
+import cv2
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+def imShow(path):
+  image = cv2.imread(path)
+  height, width = image.shape[:2]
+  resized_image = cv2.resize(image,(3*width, 3*height), interpolation = cv2.INTER_CUBIC)
+
+  fig = plt.gcf()
+  fig.set_size_inches(18, 10)
+  plt.axis("off")
+  plt.imshow(cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB))
+  plt.show()
+
+# Esta la usamos para subir archivos
+def upload():
+  from google.colab import files
+  uploaded = files.upload() 
+  for name, data in uploaded.items():
+    with open(name, 'wb') as f:
+      f.write(data)
+      print ('saved file', name)
+
+# Esta la usamos para bajar archivos
+def download(path):
+  from google.colab import files
+  files.download(path)
